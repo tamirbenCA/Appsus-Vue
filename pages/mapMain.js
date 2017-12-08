24039e940a76622622a0159d0ed4752d6cf7051a
Ben Tamir <tamirben@gmail.com>
2017-12-08 19:36:53 +0200

    ben multi markers

--- pages/mapMain.js
+++ pages/mapMain.js
@@ -1,17 +1,34 @@
+import mapService from '../services/mapService.js';
 import navBar from '../cmp/navBar.js'
 
 export default {
     template: `
-        <section>
-        <navBar></navBar>
-        <h1>MAP APP</h1>
-
-
-        
-        
-        </section>   
-                `,
+    <section>
+    <navBar></navBar>
     
+        <section class="mapMainContainer">
+            <!-- <div class="locationsContanier"></div> -->
+            <!-- <p id="locationError"></p> -->
+            {{ errorMsg }}    
+            <div class="map"></div>
+        </section>
+        </section>
+    `,
+    data() {
+        return {
+            errorMsg: '',
+            locations: []
+        }
+    },
+    created() {
+        mapService.getLocations() 
+        .then(locations => {
+            this.locations = locations
+        })
+    },
+    mounted() {
+        // mapService.getPosition()         
+    },
     components:{
         navBar
     },
--- services/mapService.js
+++ services/mapService.js
@@ -1,133 +1,220 @@
-
-const GOOGLE_API_KEY = 'AIzaSyCkGXo73iO3SNrjIp9hxptFfE5duOCgKk4'
+const GOOGLE_API_KEY = 'AIzaSyCkGXo73iO3SNrjIp9hxptFfE5duOCgKk4';
 var gLat;
 var gLng;
-var gUrl;
+var gUrl; // DO WE NEED THIS?
 
 var locations = [{
     id: 1,
+    name: 'Current Location',
+    description: '',
+    photo: '',
+    lat: 32.1047547,
+    lng: 34.8197589,
+    tag: 'current'
+},
+{
+    id: 2,
     name: 'Arnold',
     description: '',
-    photos: '../img/locations/arnold.jpg',
+    photo: '../img/locations/arnold.jpg',
     lat: 32.1047547,
     lng: 34.8197589,
     tag: 'restaurant'
 },
 {
-    id: 2,
-    name: 'Asuta',
+    id: 3,
+    name: 'Assuta',
     description: '',
-    photos: '../img/locations/asuta.jpg',
-    lat: 34.8307266,
+    photo: '../img/locations/asuta.jpg',
+    lat: 32.1079742,
     lng: 34.8307266,
     tag: 'hospital'
 },
 {
-    id: 3,
-    name: 'david yalin',
+    id: 4,
+    name: 'David Yalin',
     description: '',
-    photos: '../img/locations/david-yalin.jpg',
+    photo: '../img/locations/david-yalin.jpg',
     lat: 32.1116409,
     lng: 34.8197052,
+    tag: 'school'
+},
+{
+    id: 5,
+    name: 'Coding Academy',
+    description: '',
+    photo: '',
+    lat: 32.0878925,
+    lng: 34.8030375,
     tag: 'school'
 }]
 
 
-function createNewLocation() {
-    var newLoation = _emptyLocation();
-    locations.push(newLoation);
-    return newLoation.id + '/new';
-
+getPosition()
+function getPosition() {
+    navigator.geolocation.getCurrentPosition(showLocation, handleLocationError);
 }
 
-function _emptyLocation() {
-    return {
-        id: _getNextId(),
-        name: '',
-        description: '',
-        photos: '',
-        lat: '',
-        lng: '',
-        tag: ''
+function showLocation(position) {
+    locations[0].lat = position.coords.latitude
+    locations[0].lng = position.coords.longitude
+    initMap();
+}
+
+function initMap() {
+    var currLocation = { lat: locations[0].lat , lng: locations[0].lng };
+    var map = new google.maps.Map(document.querySelector('.map'), {
+        zoom: 15,
+        center: currLocation
+    });
+    locations.forEach(location => {
+        var marker = new google.maps.Marker({
+            position: { lat: location.lat , lng: location.lng },
+            map: map,
+            title: location.name
+        });
+        console.log('location:', location)
+    });
+}
+
+function handleLocationError(error) {
+    switch (error.code) {
+        case 0:
+            this.errorMsg = "There was an error while retrieving your location: " + error.message;
+            break;
+        case 1:
+            this.errorMsg = "The user didn't allow this page to retrieve a location.";
+            break;
+        case 2:
+            this.errorMsg = "The browser was unable to determine your location: " + error.message;
+            break;
+        case 3:
+            this.errorMsg = "The browser timed out before retrieving the location.";
+            break;
     }
 }
 
-function _getNextId() {
-    var maxId = locations.reduce((acc, note) => {
-        return (location.id > acc) ? location.id : acc
-    }, 0);
-    return maxId + 1;
-}
-
 function getLocations() {
-    return new Promise((resolve, reject) => { resolve(locations) });
-    // return Promise.reject();
+    // return new Promise((resolve, reject) => { resolve(locations) });
     return Promise.resolve(locations);
-}
-
-function showMyLocation() {
-
-    var elLocation = document.querySelector('input').value;
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${elLocation}&key=AIzaSyBmuWYnO-sINriMTx43J8ZUWho25YphaIs`)
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            var lat = data.results[0].geometry.location.lat;
-            var lng = data.results[0].geometry.location.lng;
-            getGeoLocation(lat, lng);
-            initMap(lat, lng)
-        })
+    // return Promise.reject();
 }
 
 
-function getGeoLocation(lat, lng) {
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyBmuWYnO-sINriMTx43J8ZUWho25YphaIs`)
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            var address = data.results[0].formatted_address;
-            document.querySelector('.pst').innerText = address;
-        })
+
+export default {
+    getLocations,
 }
 
 
-function saveLocation(note) {
-    return new Promise((resolve, reject) => {
-        if (location.id) {
-            var locationToUpdateIdx = locations.findIndex(currLocation => currLocation.id === location.id)
-            locations.splice(locationToUpdateIdx, 1, note);
-        }
-        else {
-            location.id = _getNextId();
-            locations.push(location);
-        }
+// ***********************************************************************
 
-        resolve()
-        // reject()
-    });
-
-}
-
-
-function deleteLocationChosen(locationId) {
-    return new Promise((resolve, reject) => {
-        var locationIdx = locations.findIndex(location => location.id === locationId)
-        locations.splice(locationId, 1);
-        resolve()
-    });
-}
+// function search() {
+//     var searchInput = document.querySelector('.searchInput');
+    
+//     if (searchInput.value) {
+//         var input = document.querySelector('.searchInput').value;
+//         axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${input}&key=AIzaSyD7eT89AbQfxKhzxEKg_lah7h0MnX_9dZc`)
+//         .then(function (res) {
+//          var latUser = res.data.results[0].geometry.location.lat;   
+//          var lngUser = res.data.results[0].geometry.location.lng;   
+//         initMap(latUser, lngUser);
+//         });
+//     }
+// }
 
 
-function getLocationById(locationId) {
-    return new Promise((resolve, reject) => {
-        var foundLocation = locations.find(location => location.id === +locationId)
-        if (foundLocation) resolve(foundLocation)
-        else reject();
-    })
+// function createNewLocation() {
+//     var newLoation = _emptyLocation();
+//     locations.push(newLoation);
+//     return newLoation.id + '/new';
 
-}
+// }
+
+// function _emptyLocation() {
+//     return {
+//         id: _getNextId(),
+//         name: '',
+//         description: '',
+//         photo: '',
+//         lat: '',
+//         lng: '',
+//         tag: ''
+//     }
+// }
+
+// function _getNextId() {
+//     var maxId = locations.reduce((acc, note) => {
+//         return (location.id > acc) ? location.id : acc
+//     }, 0);
+//     return maxId + 1;
+// }
+
+
+// function showMyLocation() {
+
+//     var elLocation = document.querySelector('input').value;
+//     fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${elLocation}&key=AIzaSyBmuWYnO-sINriMTx43J8ZUWho25YphaIs`)
+//         .then((res) => {
+//             return res.json();
+//         })
+//         .then((data) => {
+//             var lat = data.results[0].geometry.location.lat;
+//             var lng = data.results[0].geometry.location.lng;
+//             getGeoLocation(lat, lng);
+//             initMap(lat, lng)
+//         })
+// }
+
+
+// function getGeoLocation(lat, lng) {
+//     fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyBmuWYnO-sINriMTx43J8ZUWho25YphaIs`)
+//         .then((res) => {
+//             return res.json();
+//         })
+//         .then((data) => {
+//             var address = data.results[0].formatted_address;
+//             document.querySelector('.pst').innerText = address;
+//         })
+// }
+
+
+// function saveLocation(note) {
+//     return new Promise((resolve, reject) => {
+//         if (location.id) {
+//             var locationToUpdateIdx = locations.findIndex(currLocation => currLocation.id === location.id)
+//             locations.splice(locationToUpdateIdx, 1, note);
+//         }
+//         else {
+//             location.id = _getNextId();
+//             locations.push(location);
+//         }
+
+//         resolve()
+//         // reject()
+//     });
+
+// }
+
+
+// function deleteLocationChosen(locationId) {
+//     return new Promise((resolve, reject) => {
+//         var locationIdx = locations.findIndex(location => location.id === locationId)
+//         locations.splice(locationId, 1);
+//         resolve()
+//     });
+// }
+
+
+// function getLocationById(locationId) {
+//     return new Promise((resolve, reject) => {
+//         var foundLocation = locations.find(location => location.id === +locationId)
+//         if (foundLocation) resolve(foundLocation)
+//         else reject();
+//     })
+
+// }
+
 
 // function getPosition() {
 //     if (!navigator.geolocation) {
