b5393fc94df7761f7fcae5b45c64c1119a13edb9
Ben Tamir <tamirben@gmail.com>
2017-12-10 13:46:56 +0200

    ben https

--- pages/noteMain.js
+++ pages/noteMain.js
@@ -4,28 +4,32 @@ import navBar from '../cmp/navBar.js'
 export default {
     template: `
         <section>
-        <navBar></navBar>
-        <button class="note-top-button" @click="addNote"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-        <button class="note-top-button" @click="sort('date')"><i class="fa fa-sort-amount-desc" aria-hidden="true"></i> Date</button>
-        <button class="note-top-button" @click="sort('priority')"><i class="fa fa-sort-numeric-asc" aria-hidden="true"></i> Priority</button>
-            <section class="containor">
-                <div v-for="note in notes" class="note-outer">
-                    <router-link :to="'/note/' + note.id"><div class="note">
-                        <div class="note-header">
-                            <span class="title"> {{note.title}} </span>
-                            <span :style="{color: note.color}">Priority: {{note.priority}} </span>
-                        </div>
-                        <div class="note-main">
-                            {{note.text}}
-                            <img v-if="note.image" :src="'../img/' + note.image" />
-                        </div>
-                        <div class="note-footer">
-                            <!-- <button class="deleteOnNote" @click="deleteNote(note.id)">🗑</button><br> -->
-                            <i class="fa fa-trash-o" aria-hidden="true" @click="deleteNote(note.id)"></i>
-                        </div>
-                    </div></router-link>
-                </div>            
-            </section>   
+            <navBar></navBar>
+            <div class="note-main">
+                <section class="note-header">
+                    <button class="compose-mail-button" @click="addNote"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+                    <button class="note-top-button" @click="sort('date')"><i class="fa fa-sort-amount-desc" aria-hidden="true"></i> Date</button>
+                    <button class="note-top-button" @click="sort('priority')"><i class="fa fa-sort-numeric-asc" aria-hidden="true"></i> Priority</button>
+                </section>
+                <section class="containor">
+                    <div v-for="note in notes" class="note-outer">
+                        <router-link :to="'/note/' + note.id"><div class="note">
+                            <div class="note-header">
+                                <span class="title"> {{note.title}} </span>
+                                <span :style="{color: note.color}">Priority: {{note.priority}} </span>
+                            </div>
+                            <div class="note-main">
+                                {{note.text}}
+                                <img v-if="note.image" :src="'../img/' + note.image" />
+                            </div>
+                            <div class="note-footer">
+                                <!-- <button class="deleteOnNote" @click="deleteNote(note.id)">🗑</button><br> -->
+                                <i class="fa fa-trash-o" aria-hidden="true" @click="deleteNote(note.id)"></i>
+                            </div>
+                        </div></router-link>
+                    </div>
+                </section>   
+            </div>            
         </section>   
                 `,
                 
