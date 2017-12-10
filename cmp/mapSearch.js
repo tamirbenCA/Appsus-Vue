f18b18543c62853ea5e232c5a7d44771fb10e6ce
Ben Tamir <tamirben@gmail.com>
2017-12-10 13:53:48 +0200

    ben

--- cmp/mapSearch.js
+++ cmp/mapSearch.js
@@ -1,13 +1,8 @@
 export default {
     template: `
         <section class="search-map">
-<<<<<<< HEAD
-            <div class="search-container">
-                <input class="search-box" type="search" placeholder="Search Location" v-model="term" @keyup.enter="searchLocation" @blur="searchLocation">
-=======
             <div>
                 <input class="search-box" id="searchTextField" type="search" placeholder="Search Location" v-model="term" @keyup.enter="searchLocation" @blur="searchLocation">
->>>>>>> 321647f06bb07f62b3364fcba813fccd30154910
             </div>
             <div class="search-buttons">
               <button class="search-bar-button" @click="displaySavedLocations">Display Saved Locations</button>
